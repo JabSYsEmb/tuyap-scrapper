a4347e891737ef0ac8f3717374465f3c6f1fc5ed
JabSysEmb <abduljabbar.sha97@gmail.com>
2022-10-01 18:09:50 +0300

    work on scrapping

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const axios = require("axios");
 const jsdom = require("jsdom");
+const fs    = require("fs");
 
 
 const main = "div.filter-list__item > div > table > tbody > tr" // css selector
 
-const url = "https://maktekfuari.com/katilimci-listesi" // website
+const fuar_name = "maktek"
+
+const url_fuar = "https://maktekfuari.com/katilimci-listesi?page=" // website
 
 
 const document_builder = (html_as_string) =>
@@ -17,17 +20,32 @@ const get_company_info = (company_instance) => {
 	const location = company_instance[1].trim();
 	const contact = company_instance[4].trim().split(":").pop().trim();
 	const website = "https:" + company_instance[5].trim().split(":").pop().trim();
-	const company = {name, location, contact, website}
-	return JSON.stringify(company)
+	if(name && location && contact && website) {
+		const company = {name, location, contact, website}
+		fs.appendFile(`./${fuar_name}.json`, JSON.stringify(company), (err) => {
+			if (err) throw err;
+			console.log(`${name}`);
+		})
+	}
 }
 
-axios.get(url)
-	.then(response => response.data)
-	.then(document_builder)
-	.then(main_selector)
-	.then(data => data)
-	.then(data => data.filter((_,idx) => idx % 3 === 0))
-	.then(objs => objs.map(item => item.textContent.trim().split("\n")))
-	.then(objz => objz.map(item => get_company_info(item)))
-	.then(objs => console.log('[' + objs.join(",") + ']')) // this is temperarory use 'fs' to create and write inside a json file
-	.catch((err) => console.error("Error:",err))
+async function companies(url){
+	await axios.get(url)
+		.then(response => response.data)
+		.then(document_builder)
+		.then(main_selector)
+		.then(data => data.filter((_,idx) => idx % 3 === 0))
+		.then(objs => objs.map(item => item.textContent.trim().split("\n")))
+		.then(objz => objz.map(item => get_company_info(item)))
+		.catch((err) => console.error("Error:",err))
+		.finally(() => console.log(""))
+}
+
+function fuari_companies(url,idx){
+	while( idx > 0 ){
+		companies(`${url}${idx}`);
+		idx--;
+	}
+}
+
+fuari_companies(url_fuar,37);
